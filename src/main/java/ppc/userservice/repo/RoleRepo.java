8b87b0a33e83dbc122eb96b4be237a00d2fc88a1
Nikolai Kocev <nikolai_kocev@hotmail.com>
2022-11-21 11:43:56 +0100

    Annotated the repos with @Repository

--- src/main/java/ppc/userservice/repo/RoleRepo.java
+++ src/main/java/ppc/userservice/repo/RoleRepo.java
@@ -1,8 +1,10 @@
 package ppc.userservice.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ppc.userservice.domain.Role;
 
+@Repository
 public interface RoleRepo extends JpaRepository<Role, Long> {
     Role findByName(String name);
 }
--- src/main/java/ppc/userservice/repo/UserRepo.java
+++ src/main/java/ppc/userservice/repo/UserRepo.java
@@ -1,8 +1,10 @@
 package ppc.userservice.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ppc.userservice.domain.User;
 
+@Repository
 public interface UserRepo extends JpaRepository<User, Long> {
     User findByUsername(String username);
 }
